8f2a97c224cde755a76a4cac4467df1b9be3bc10
Poisson <imatheusfsantos@gmail.com>
2015-11-09 00:35:51 -0200

    VAMO Q VAMO LEKADA O DANONÃO TÁ LIBERADO

---
+++ app/src/main/java/com/orochi/utfpr/levaeu/CoordenadaGPS.java
@@ -0,0 +1,30 @@
+package com.orochi.utfpr.levaeu;
+import java.util.*;
+
+/**
+ * 
+ */
+public class CoordenadaGPS {
+
+    /**
+     * Default constructor
+     */
+    public CoordenadaGPS() {
+    }
+
+    /**
+     * 
+     */
+    private double latitude;
+
+    /**
+     * 
+     */
+    private double longitude;
+
+    /**
+     * 
+     */
+    private double precisao;
+
+}
